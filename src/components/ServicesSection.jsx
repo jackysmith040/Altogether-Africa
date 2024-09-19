7ab25a2f0b3a0d3f8cdd92e6ed0b350ad9e404c6
K-0bby <k.hoenyefia@gmail.com>
2024-09-19 12:40:21 +0000

    update

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -82,7 +82,7 @@ const ServicesSection = () => {
         </div>
       </div>
 
-      <div className='z-20 relative grid grid-cols-1 gap-8 px-12 lg:-mt-[560px]'>
+      <div className='z-20 relative grid grid-cols-1 gap-8 px-12 lg:-mt-[500px]'>
           <div className='flex flex-col items-center lg:items-end'>
             <div className='bg-primary flex items-center lg:mx-24 justify-center w-[200px] h-[150px] text-white rounded-lg'>
               <h3 className='text-4xl font-bold mb-4'>3</h3>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import './index.css'
+import 'normalize.css'
 import App from './App'
 import GetInvolved from './pages/GetInvolved';
 import Donate from './pages/Donate';
